6c76be126b5c88afe358f0a6141438a6e4285bd4
Dimm <814885777@qq.com>
2025-01-19 23:23:12 +0800

    Add

---
+++ LeetCode/Classisc/Classics_66.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Classisc
+{
+    public class Classics_66
+    {
+        public int[] PlusOne(int[] digits)
+        {
+            
+            int len = digits.Length;
+            int last = digits[len-1];
+
+            for (int i = len-1; i > 0 ; i--)
+            {
+                digits[i] = ++digits[i] % 10;
+                if (digits[i] != 0)
+                {
+                    return digits;
+                }
+            }
+
+            digits = new int[len + 1];
+            digits[0] = 1;
+
+            return digits;
+        }
+    }
+}
---
+++ UnitTest/UnitTest.cs
@@ -0,0 +1,16 @@
+using LeetCode.Classisc;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitTest
+    {
+        [TestMethod]
+        public void Test_Claaics100()
+        {
+            Classics_66 classics = new Classics_66();
+            int[] res = classics.PlusOne(new[] { 9,9 });
+            
+        }
+    }
+}
